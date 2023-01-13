7a2d8ef3df757e210b560594bedd92212a38c5d0
PhoenixKing2501 <utsavbasu@outlook.com>
2023-01-13 22:34:50 +0530

    part 1

--- Week 1/Part 1/Assgn1_1_4.sh
+++ Week 1/Part 1/Assgn1_1_4.sh
@@ -1,1 +1,25 @@
 #! /bin/sh
+
+function gcd() {
+	local a=$1
+	local b=$2
+	local t
+	while [[ $b > 0 ]]; do
+		let t=a
+		let a=b
+		let b=$((t % b))
+	done
+	echo $a
+}
+
+function lcm() {
+	echo $(($1 / $(gcd $1 $2) * $2))
+}
+
+res=1
+
+for i in $(rev $1); do
+	res=$(lcm $i $res)
+done
+
+echo $res
