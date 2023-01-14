06b5f81fa0b43dbaedc221f5a407c3a08fa27ab1
PhoenixKing2501 <utsavbasu@outlook.com>
2023-01-14 15:17:49 +0530

    part 2

---
+++ Week 1/Part 2/Assgn1_2_4.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+black=$(sed -zEe 's/\s/|/gm' fruits.txt)
+sed usernames.txt -Ee "/^(.*(${black:0:(-1)}).*|.{1,4}|.{21,})$/I! s/^[a-z][a-z0-9]*[0-9]+[a-z0-9]*$/YES/gmi" -e 't' -Ee "s/.*/NO/gmi" >validation_results.txt
