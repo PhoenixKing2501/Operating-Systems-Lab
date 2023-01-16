5bc3b3dbb8c2417a8e7dd0228090c772e5248f93
anamitra137 <anamitra137@gmail.com>
2023-01-16 20:04:57 +0530

    3 echos to see progress

--- Week 1/Part 6/Assgn1_6_4.sh
+++ Week 1/Part 6/Assgn1_6_4.sh
@@ -1,1 +1,47 @@
-#! /bin/sh
+#! /bin/bash
+
+N=1000000
+declare -a isprime
+isprime[1]=0
+
+
+for (( i=2; i<=N; i++ ))
+do
+    isprime[i]=1
+done
+for(( i=2; i<=N; i++ ))
+do
+    if [ ${isprime[i]} -eq 1 ]
+    then
+        for(( j=$(( i * i )); j<=N; j=$(( j + i )) ))
+        do
+            isprime[j]=0
+        done
+    fi
+done
+
+
+echo "sieve done"
+
+primes=" "
+declare -a indices
+indices[1]=0
+for (( i=2; i<=N; i++ ))
+do
+    if [ ${isprime[i]} -eq 1 ]
+    then
+        primes+="$i "
+        indices[i]=$(( ${#primes} - 1 ))
+    else
+        indices[i]=${indices[i-1]}
+    fi
+done
+
+echo "primes list done"
+
+cat input.txt | while read n
+do
+    echo ${primes:1:${indices[n]}}
+done > output.txt
+
+echo "all done"
