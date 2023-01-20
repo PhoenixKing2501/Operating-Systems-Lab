4f0d80eb9593ccb9bf409c60fa7d8accef08fa58
anamitra137 <anamitra137@gmail.com>
2023-01-20 16:40:09 +0530

    small change in regex

--- Week 1/Part 7/Assgn1_7_4.sh
+++ Week 1/Part 7/Assgn1_7_4.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-chars=( {a..z} )
+chars=( {A..Z} )
 
 mkdir -p "$2"
 for (( i=0; i<26; i++ ))
@@ -10,5 +10,5 @@ done
 
 for (( i=0; i<26; i++ ))
 do
-    grep -horiw "${chars[i]}.*" "$1" | sort > "$2/${chars[i]}.txt"
+    grep -hori "^${chars[i]}.*" "$1" | sort > "$2/${chars[i]}.txt"
 done
