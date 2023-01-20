de447dd50d7472c18685de28ab53271d2aef84cc
PhoenixKing2501 <utsavbasu@outlook.com>
2023-01-20 22:52:32 +0530

    fixing 9

--- Week 1/Part 9/Assgn1_9_4.sh
+++ Week 1/Part 9/Assgn1_9_4.sh
@@ -1,22 +1,2 @@
-#! /bin/sh
-gawk '{
-                arr1[$1]++;
-                arr2[$2]++
-      }
-      END {
-                for (ele in arr2)
-                {
-                  print ele" "arr2[ele] | "sort -k2,2nr"
-                }
-                for (ele in arr1) 
-                    {
-                        if(arr1[ele]==1)
-                            cnt++;
-                        else 
-                            s=s""ele"\n";
-                    }
-                   
-                   print s""cnt
-                  
-        }
-        ' $1
+#! /bin/gawk -f
+gawk '{ arr1[$1]++;arr2[$2]++ } END {for (ele in arr2){  print ele" "arr2[ele] | "sort -k2,2nr"}for (ele in arr1) {if(arr1[ele]==1)cnt++;else s=s""ele"\n";}print s""cnt  }' $1
