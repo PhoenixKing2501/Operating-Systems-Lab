0e324fa13a6e8fdb97fde99c786c2f6c3046b332
PhoenixKing2501 <utsavbasu@outlook.com>
2023-01-20 21:25:42 +0530

    formatted 7

--- Week 1/Part 7/Assgn1_7_4.sh
+++ Week 1/Part 7/Assgn1_7_4.sh
@@ -1,14 +1,12 @@
 #! /bin/sh
 
-chars=( {A..Z} )
+chars=({A..Z})
 
 mkdir -p "$2"
-for (( i=0; i<26; i++ ))
-do
-    touch "$2/${chars[i]}.txt"
+for ((i = 0; i < 26; i++)); do
+	touch "$2/${chars[i]}.txt"
 done
 
-for (( i=0; i<26; i++ ))
-do
-    grep -hori "^${chars[i]}.*" "$1" | sort > "$2/${chars[i]}.txt"
+for ((i = 0; i < 26; i++)); do
+	grep -hori "^${chars[i]}.*" "$1" | sort >"$2/${chars[i]}.txt"
 done
