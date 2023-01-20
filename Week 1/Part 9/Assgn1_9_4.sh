b324673b3b1ec5521192b79601fce7eb597c4efa
PhoenixKing2501 <utsavbasu@outlook.com>
2023-01-21 00:33:47 +0530

    output order ok in 9 now

--- Week 1/Part 9/Assgn1_9_4.sh
+++ Week 1/Part 9/Assgn1_9_4.sh
@@ -1,2 +1,20 @@
-#! /bin/gawk -f
-gawk '{ arr1[$1]++;arr2[$2]++ } END {for (ele in arr2){  print ele" "arr2[ele] | "sort -k2,2nr"}for (ele in arr1) {if(arr1[ele]==1)cnt++;else s=s""ele"\n";}print s""cnt  }' $1
+#! /bin/bash
+gawk '{
+		arr1[$1]++
+		arr2[$2]++
+	  } END {
+		for (ele in arr2)
+		{
+			l=l ele" "arr2[ele]"\n" 
+		}
+		for (ele in arr1)
+		{
+			if(arr1[ele]==1)
+				cnt++;
+			else
+				s=s""ele"\n";
+		}
+
+		system("echo -e \""l"\" | sort -k2,2nr")
+		print s""cnt
+	  }' $1
