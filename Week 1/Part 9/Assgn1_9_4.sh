b1d6952c33503fd26c1ff14695db4d2ef514dfb5
swarup3204 <swarupksms@gmail.com>
2023-01-22 17:46:13 +0530

    1.5 and 1.9

--- Week 1/Part 9/Assgn1_9_4.sh
+++ Week 1/Part 9/Assgn1_9_4.sh
@@ -1,20 +1,4 @@
-#! /bin/bash
-gawk '{
-		arr1[$1]++
-		arr2[$2]++
-	  } END {
-		for (ele in arr2)
-		{
-			l=l ele" "arr2[ele]"\n" 
-		}
-		for (ele in arr1)
-		{
-			if(arr1[ele]==1)
-				cnt++;
-			else
-				s=s""ele"\n";
-		}
-
-		system("echo -e \""l"\" | sort -k2,2nr")
-		print s""cnt
-	  }' $1
+res=$(gawk '{arr1[$1]++;arr2[$2]++}END{for(ele in arr2)l=l ele" "arr2[ele]"\n";for(ele in arr1)if(arr1[ele]==1)cnt++;else s=s""ele"\n";print l|"sort -k2,2nr>out.txt";print s""cnt}' $1)
+cat out.txt
+echo $res|sed 's/ /\n/g'
+rm out.txt
