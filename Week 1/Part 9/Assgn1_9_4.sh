df89901a84b1b0b533c5bce1ae7eb450b016c6ee
swarup3204 <swarupksms@gmail.com>
2023-01-22 18:53:27 +0530

    Revert "1.5 and 1.9"
    This reverts commit b1d6952c33503fd26c1ff14695db4d2ef514dfb5.

--- Week 1/Part 9/Assgn1_9_4.sh
+++ Week 1/Part 9/Assgn1_9_4.sh
@@ -1,4 +1,20 @@
-res=$(gawk '{arr1[$1]++;arr2[$2]++}END{for(ele in arr2)l=l ele" "arr2[ele]"\n";for(ele in arr1)if(arr1[ele]==1)cnt++;else s=s""ele"\n";print l|"sort -k2,2nr>out.txt";print s""cnt}' $1)
-cat out.txt
-echo $res|sed 's/ /\n/g'
-rm out.txt
+#! /bin/bash
+gawk '{
+		arr1[$1]++
+		arr2[$2]++
+	  } END {
+		for (ele in arr2)
+		{
+			l=l ele" "arr2[ele]"\n" 
+		}
+		for (ele in arr1)
+		{
+			if(arr1[ele]==1)
+				cnt++;
+			else
+				s=s""ele"\n";
+		}
+
+		system("echo -e \""l"\" | sort -k2,2nr")
+		print s""cnt
+	  }' $1
