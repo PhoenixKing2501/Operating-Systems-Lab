845f614e46db85a6d2c84dde3a16262a8b61280a
PhoenixKing2501 <utsavbasu@outlook.com>
2023-02-04 21:12:19 +0530

    add test file

--- Week 2/src/test.c
+++ Week 2/src/test.c
@@ -1,11 +1,15 @@
+// Just to test the shell with a simple program, We wrote this:
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/poll.h>
 
 int main()
 {
 	char *line = NULL;
 	size_t bufsize = 0;
 	getdelim(&line, &bufsize, EOF, stdin);
+	poll(NULL, 0, 5000);
 	printf("%s", line);
 	free(line);
 }
